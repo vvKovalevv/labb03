e66daca494931605f426435e9410933be7b90e10
vvKovalevv <62720961+vvKovalevv@users.noreply.github.com>
2020-05-07 23:52:01 +0300

    Add files via upload

---
+++ svg.cpp
@@ -0,0 +1,61 @@
+ #include "svg.h"
+ #include <vector>
+ #include <iostream>
+ using namespace std;
+ void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void
+svg_end() {
+
+    cout << "</svg>\n";
+}
+void svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
+}
+void svg_rect(double x, double y, double width, double height,string stroke, string fill)
+{
+    cout<< "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<< "' fill='"<<fill<<"'/>";
+}
+void show_histogram_svg(const vector<size_t>& bins, size_t& number_count)
+{
+    const size_t MAX_ASTERISK=30;
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    const auto WIDTH_TO_PROCENT = 370;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    unsigned max_count=0;
+    for (size_t b : bins) /* мы присваиваем значение количества элементов */
+    {
+        if(max_count<b)
+            max_count=b;
+    }
+    for (size_t bin : bins)
+    {
+        size_t height=30;
+        if(max_count>MAX_ASTERISK) /* Если количество будет больше 35, то уменьшаем масштаб*/
+            height=MAX_ASTERISK*((static_cast<double>(bin))/max_count); /* это для того,чтобы было дробное число , получаем, используя "static_cast<double>"*/
+        else
+        {
+            height=bin;
+        }
+        height = BLOCK_WIDTH * height;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, height, BIN_HEIGHT, "black", "yellow");
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
