b24f3e397a30e2d5e11b32844bbbd409de4e6b04
artur123 <artur123@gmail.com>
2020-05-08 12:02:01 +0300

    code:3.1.1

--- main.cpp
+++ main.cpp
@@ -7,13 +7,12 @@ using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
-vector<double>
-input_numbers(size_t count)
+vector<double> input_numbers(istream& in, size_t count)
 {
-    vector<double> result(count);
-    for (size_t i = 0; i < count; i++)
+    vector <double> result(count);
+    for (int i=0; i<count; i++)
     {
-        cin >> result[i];
+        in>>result[i];
     }
     return result;
 
@@ -47,9 +46,11 @@ int main()
 
     cerr << "Enter number count: ";
 
-    cin >> number_count;
+    vector<double> input_numbers(istream& in, size_t count);
 
-    const auto numbers=input_numbers(number_count);
+
+
+    const auto numbers=input_numbers(cin,number_count);
     size_t bin_count;
 
     cerr << "Enter column count: ";
