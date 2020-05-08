9aee2629412016453c80365a78501211b37d25d1
artur123 <artur123@gmail.com>
2020-05-08 13:24:40 +0300

    code:дописал read_input()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
-#include "histogram.h"
 #include <vector>
+#include <conio.h>
+#include "histogram.h"
 #include "svg.h"
+
 using namespace std;
 
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 
 vector<double> input_numbers(istream& in, size_t count)
 {
@@ -18,50 +21,56 @@ vector<double> input_numbers(istream& in, size_t count)
 
 }
 
-vector<size_t>
-make_histogram(const vector <double> &numbers,double &max, double &min, size_t &bin_count)
-{
+Input
+read_input(istream& in) {
+    Input data;
 
-    vector<size_t>bins(bin_count,0); /* массив из индексов корзин */
-    for(double x : numbers) /* мы каждому x присваиваем последовательно каждый элемент массива "numbers" */
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
 
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
 
-    {
-        size_t bin_index=(size_t)((x-min)*bin_count/(max-min));
-        if (bin_index==bin_count) /* индекс корзины "bin_index"*/
-        {
-            bin_index=bin_index-1; /* нумирация идет с 0*/
-        }
-        bins[bin_index]++;
-
-    }
-    return bins;
+     cerr<<"bin_count=";
+     size_t bin_count;
+     in>> bin_count;
+  double max=0, min=0;
+ find_minmax(data.numbers,max,min);
+    return data;
 }
+
+
+vector<size_t> make_histogram (size_t count,const vector<double>& numbers)
+{ double min=0;
+double max=0;
+find_minmax(numbers,min,max);
+vector<size_t> bins(count);
+for (double number : numbers) {
+size_t bin = (size_t)((number - min) / (max - min) * count);
+if (bin == count)
+{
+bin--;
+}
+bins[bin]++;
+}
+return bins;
+}
+
 
 
 int main()
 {
+    double left;
+    double baseline;
+    string text;
+    size_t number_count, bin_count;
 
-    size_t number_count;
+    Input data=read_input(cin);
 
-    cerr << "Enter number count: ";
-
-    vector<double> input_numbers(istream& in, size_t count);
-
-
-
-    const auto numbers=input_numbers(cin,number_count);
-    size_t bin_count;
-
-    cerr << "Enter column count: ";
-
-    cin >> bin_count;
-
-    double min, max;
-    find_minmax(numbers, min, max);
+    const auto bins=make_histogram(data.bin_count,data.numbers);
+    show_histogram_svg(bins,data.bin_count);
+    return 0;}
 
 
-     const auto bins=make_histogram(numbers, max, min, bin_count);
-     show_histogram_svg(bins,number_count);
-    return 0;
-}
+
